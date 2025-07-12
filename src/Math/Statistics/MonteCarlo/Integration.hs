d286de05beabd6bd07ef1d49a195140a4f139235
Hiromi ISHII <konn.jinro@gmail.com>
2025-07-12 19:48:07 +0900

    doctesting

--- src/Math/Statistics/MonteCarlo/Integration.hs
+++ src/Math/Statistics/MonteCarlo/Integration.hs
@@ -1,11 +1,14 @@
+{-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Math.Statistics.MonteCarlo.Integration (
   integrator,
   integrate,
+  fanIntegrator,
 ) where
 
 import Data.Generics.Labels ()
+import Linear
 import Math.Statistics.MonteCarlo.Sampler
 import Math.Statistics.RandomVar qualified as RVar
 import System.Random.Stateful
@@ -19,14 +22,29 @@ integrator (lb, ub) f
     space = RVar.uniformR (lb, ub)
     estimator = estimateBy f
 
+{- |
+Integrate a unary function over a specified interval using Monte Carlo method.
+
+>>> integrate (0, 1) (\x -> sqrt $ 1 - x * x)  1000 (mkStdGen 42)
+0.7820929243459432
+
+>>> integrate (0, 1) (\x -> sqrt $ 1 - x * x)  10000 (mkStdGen 42)
+0.7837574992199501
+
+>>> integrate (0, 1) (\x -> sqrt $ 1 - x * x)  100000 (mkStdGen 42)
+0.7859682863878684
+-}
 integrate :: (RandomGen g) => (Double, Double) -> (Double -> Double) -> Int -> g -> Double
 integrate bounds f = evalMonteCarlo (integrator bounds f)
 
--- >>> integrate (0, 1) (\x -> sqrt $ 1 - x * x)  1000 (mkStdGen 42)
--- 0.7820929243459432
+{- |
+Integrate over the fan-region defined by \(x^2 + y^2 < 1, x,y > 0\).
 
--- >>> integrate (0, 1) (\x -> sqrt $ 1 - x * x)  10000 (mkStdGen 42)
--- 0.7837574992199501
-
--- >>> integrate (0, 1) (\x -> sqrt $ 1 - x * x)  100000 (mkStdGen 42)
--- 0.7859682863878684
+>>> 2 * pi * evalMonteCarlo (fanIntegrator (\(V2 x y) -> sqrt $ 1 - x * x - y * y)) 100000 (mkStdGen 42)
+4.180798994170995
+-}
+fanIntegrator :: (V2 Double -> Double) -> MonteCarlo (V2 Double) Double
+fanIntegrator f = MonteCarlo {space, estimator}
+  where
+    space = V2 <$> RVar.uniformR (0, 1) <*> RVar.uniformR (0, 1)
+    estimator = filtered ((< 1) . quadrance) (estimateBy f)
--- src/Math/Statistics/MonteCarlo/Pi.hs
+++ src/Math/Statistics/MonteCarlo/Pi.hs
@@ -20,19 +20,21 @@ piSampleSpace :: RVar (V2 Double)
 piSampleSpace = V2 <$> RVar.uniformR (0, 1) <*> RVar.uniformR (0, 1)
 
 piEstimator :: Estimator (V2 Double) Double
-piEstimator = 4 * count ((<= 1) . norm)
+piEstimator = 4 * count ((<= 1) . quadrance)
 
 calcPi :: (RandomGen g) => Int -> g -> (Double, g)
 calcPi = runMonteCarlo piMonteCarlo
 
+{- | Calculates π by Monte Carlo method.
+
+>>> calcPi_ 10000 (mkStdGen 42)
+3.125999999999995
+
+>>> calcPi_ 1000000 (mkStdGen 42)
+3.141816000000033
+-}
 calcPi_ :: (RandomGen g) => Int -> g -> Double
 calcPi_ = evalMonteCarlo piMonteCarlo
 
 iteratePi :: (RandomGen g, Monad m) => g -> Stream (Of Double) m r
 iteratePi gen = iterateMonteCarlo piMonteCarlo gen
-
--- >>> calcPi_ 10000 (mkStdGen 42)
--- 3.125999999999995
-
--- >>> calcPi_ 1000000 (mkStdGen 42)
--- 3.141816000000033
